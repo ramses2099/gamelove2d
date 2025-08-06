8c7d6335d0816a7a295f4538b0c2cc50d8fbfdcb
ramses2099 <ramses2099@gmail.com>
2025-08-05 21:14:03 -0400

    lastes change in vector class

--- main.lua
+++ main.lua
@@ -8,23 +8,13 @@ RADIUS = 32
 W_WIDTH = 800
 W_HEIGHT = 600
 
-function is_even(n)
-  return n > 5
-end
+
 
 function love.load()
   DEBUG = true
   -- Temp
-  
-  local numbers = {1,2,3,4,5,6,7,8,10}
 
-  local even_number = Utils.filter(numbers, is_even)
 
-  Utils.dump(even_number)
-  
-  -- particle = Particle.new(W_WIDTH/2, 20)
-  -- system = ParticleSystem.new()
-  
 end
 
 function love.mousepressed(x, y, button, istouch)
@@ -40,17 +30,7 @@ function love.keypressed(key, scancode, isrepeat)
 end
 
 function love.update(dt)
-  -- particle:update(dt)
-  -- system:add(Particle.new(W_WIDTH/2, 20))
-  -- local gravity = Vec.createVector(0, 0.1)
-  -- particle:applyForce(gravity)
-
-  -- if (particle:isDead()) then
-  --   particle = Particle.new(W_WIDTH/2, 20)
-  --   print("Particle dead!")
-  -- end
-
-  -- system:update(dt)
+ 
 end
 
 function love.draw()
@@ -60,8 +40,5 @@ function love.draw()
     love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 10)
   end
 
-  -- particle:draw()
-
-  -- system:draw()
-
-end
+  
+ end
