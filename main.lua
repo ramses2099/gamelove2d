84295ddfb191e3a78698eb906ef029f1374d2cd3
ramses2099 <ramses2099@gmail.com>
2025-08-05 21:22:27 -0400

    lastes change in main

---
+++ main.lua
@@ -0,0 +1,19 @@
+_G.love = require("love")
+
+function love.load()
+    
+end
+
+function love.keypressed( key, scancode, isrepeat )
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
